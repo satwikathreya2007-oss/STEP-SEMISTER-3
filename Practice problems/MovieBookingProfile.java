140add06860250bd18a398f6ac5030d8881ba15c
Satwik Athreya <satwikathreya2007@gmail.com>
2026-09-19 06:51:13 +0530

    MovieBookingProfile class for booking management

---
+++ Practice problems/MovieBookingProfile.java
@@ -0,0 +1,40 @@
+class MovieBookingProfile{
+    private String name;
+    private boolean confirmed;
+    private String otp;
+
+    public MovieBookingProfile(){}
+
+    public MovieBookingProfile(String name){
+        this();
+        this.name=name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public boolean isConfirmed(){
+        return confirmed;
+    }
+
+    public void setConfirmed(boolean confirmed){
+        this.confirmed=confirmed;
+    }
+
+    public void setOtp(String otp){
+        this.otp=otp;
+    }
+
+    public static void main(String[] args){
+        MovieBookingProfile p=new MovieBookingProfile("Rahul Dev");
+        p.setConfirmed(true);
+        p.setOtp("4471");
+        System.out.println(p.getName());
+        System.out.println(p.isConfirmed());
+    }
+}
